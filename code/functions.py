78afd41dc7c737a77837a604cb90225f65034132
thedu7 <56830913+thedu7@users.noreply.github.com>
2019-11-02 14:55:45 +0100

    Add files via upload

--- code/functions.py
+++ code/functions.py
@@ -10,11 +10,9 @@ from bs4 import BeautifulSoup
 CONFIG
 '''
 
-data = ["Nom", "Autor", "Tematica", "Preu", "Temps de joc", "Dificultat", "Jugadors", "Idioma", "Descripcio", "Edat", "URL"]
-dato = ["","","","","","","","","","","","","","","","","","",""]
+data = ["Nom", "Autores", "Tematica", "Preu", "Temps de joc", "Dificultat", "Jugadors", "Idioma", "Descripcio", "Edat", "URL"]
 file = 'Juegos-Zacatrus.csv'
 basic_url_zacatrus = 'https://zacatrus.es/'
-
 
 '''
 FUNCTIONS
@@ -25,68 +23,90 @@ def parse_page_info(url):
     # Create a BeautifulSoup object
     soup_info = BeautifulSoup(url, 'html.parser')
     soup_info.encode("utf-8")
-        
-    dato[2] = soup_info.find(class_='price').text.strip()
     
+    # Pull all text from the class "additional-attributes-wrapper table-wrapper"
     games_infos = soup_info.find(class_='additional-attributes-wrapper table-wrapper')
-    games_list_infos = games_infos.find_all('td') 
     
-    i = 3
-    for info in games_list_infos:
+    # Check all the extracted data
+    try:
+        data[3] = soup_info.find(class_='price').text.strip()
+    except AttributeError:
+        data[3] = "No identificat"
+    
+    try:
+        data[1] = games_infos.find("td", {'data-th':'Autor'}).text.strip()
+    except AttributeError:
+        data[1] = "No identificat/s"
         
-        dato[i] = info.text.strip()
-        i += 1
-
-    dato[i] = soup_info.find(class_="product attribute description").text.strip()
-
+    try:
+        data[2] = games_infos.find("td", {'data-th':'Temática'}).text.strip()
+    except AttributeError:
+        data[2] = "No identificada"
     
+    try:
+        data[4] = games_infos.find("td", {'data-th':'Tiempo de juego'}).text.strip()
+    except AttributeError:
+        data[4] = "No identificat"
+    
+    try:
+        data[5] = games_infos.find("td", {'data-th':'Complejidad'}).text.strip()
+    except AttributeError:
+        data[5] = "No identificada"
+        
+    try:
+        data[6] = games_infos.find("td", {'data-th':'Núm. jugadores'}).text.strip()
+    except AttributeError:
+        data[6] = "No identificat"
+    
+    try:
+        data[7] = games_infos.find("td", {'data-th':'Idioma'}).text.strip()
+    except AttributeError:
+        data[7] = "No identificat"
+    
+    try:
+        data[9] = games_infos.find("td", {'data-th':'Edad'}).text.strip()
+    except AttributeError:
+        data[9] = "No identificada"
+    
+    try:
+        data[8] = soup_info.find(class_="product attribute description").text.strip()
+    except AttributeError:
+        data[8] = "No hi ha descripció"
+
+        
 def parse_main_page(html):
     
     # Create a BeautifulSoup object
     soup = BeautifulSoup(html, 'html.parser')
     soup.encode("utf-8")
     
-    #create_csv(file, data)
+    # Create_csv(file, data)
     f = csv.writer(open(file, 'w'))
     f.writerow(data)  
     
-    # Pull all text from the BodyText div
+    # Pull all text from the class 'products list items product-items'
     games_list = soup.find(class_='products list items product-items')
-    
-    # Pull text from all instances of <a> tag within BodyText div
+
+    # Pull text from all instances of <a> tag within the class 'product-item-link'
     games_list_items = games_list.find_all('a', {'class':'product-item-link'})
     
-    # Create for loop to print out all expositions informations
+    # Create for loop to print out all games informations
     for games in games_list_items:
     
         links = games.get('href')
-        dato[0] = links
+        data[10] = links
         name = games.text.strip()
-        dato[1] = name
+        data[0] = name
         
-        #crawl new urls
+        # Crawl the new games urls = 'links'
         zacatrus_crawler(links, basic_url_zacatrus, 2)
         
-        data_list = (dato[1],
-                     dato[3],
-                     dato[6],
-                     dato[2],
-                     dato[10],
-                     dato[12],
-                     dato[9],
-                     dato[15],
-                     dato[8],
-                     dato[18],
-                     dato[0])
-
-        print(data_list)
-        
-        f.writerow(data_list)
+        # Write data in csv(data)
+        f.writerow(data)
 
         
 def scrap_web_content(html, ind):
     if html is not None:
-        print("OK")
         if ind == 1:
             parse_main_page(html)    
         else:
--- code/main.py
+++ code/main.py
@@ -4,4 +4,5 @@ MAIN
 
 from functions import zacatrus_crawler
 
+#Start the crawl spider
 zacatrus_crawler('https://zacatrus.es/juegos-de-mesa.html', 'https://zacatrus.es/', 1)
